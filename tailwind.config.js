4b127f6bc8d0a325bb9e589ecf7ccbc820ddb2b0
Collins <mutaic84@gmail.com>
2022-05-20 23:32:00 +0300

    create a tailwind configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        brightRed: 'hsl(12, 88%, 59%',
-        brightRedLight: 'hsl(12, 88%, 69%',
-        brightRedSupLight: 'hsl(12, 80%, 95%',
-        darkBlue: 'hsl(228, 39%, 23%',
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 80%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
